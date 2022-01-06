11e6d8fdd3ddaacb6beacb11a035e5024da55893
VicN <vnikolaeff989@gmail.com>
2022-01-06 18:08:52 +0300

    Initial HomeWork8

---
+++ lesson8/src/main/java/ru/geekbrains/homework/service/CategoryService.java
@@ -0,0 +1,21 @@
+package ru.geekbrains.homework.service;
+
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.geekbrains.homework.entities.Category;
+import ru.geekbrains.homework.repositories.CategoryRepository;
+
+import java.util.List;
+
+@Setter
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public List<Category> findAll() {
+        return categoryRepository.findAll();
+    }
+}
